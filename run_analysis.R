12492a023208b00bdd0dbca11fded8c20af503fc
kumar16akash <kumar16akash@gmail.com>
2018-01-26 20:12:30 +0530

    Added comments to run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,4 @@
+# Getting all the datasets
 library(reshape2)
 x_train <- read.table("train/X_train.txt")
 y_train <- read.table("train/y_train.txt")
@@ -10,6 +11,8 @@ subject_test <- read.table("test/subject_test.txt")
 features <- read.table("features.txt")
 activity_labels <- read.table("activity_labels.txt")
 
+## Assigning Column names to variables in test & training dataset
+
 colnames(y_test) <- "activity_code"
 colnames(y_train) <- "activity_code"
 colnames(x_test) <- features[,2]
@@ -18,28 +21,30 @@ colnames(subject_test) <- "subject_code"
 colnames(subject_train) <- "subject_code"
 colnames(activity_labels) <- c("activity_code","activity_type")
 
-#Merge--------------------------------------
+#Merging both the train & test dataset to one.
 
 merged_test <- cbind(subject_test,y_test,x_test)
 merged_train <- cbind(subject_train,y_train,x_train)
 merged_all <- rbind(merged_train,merged_test)
 
-#Extraction------
+#Extracting the mean and standard deviation values
+
 col_Names <- colnames(merged_all)
 logical_Mean_StdDevn <- (grepl("subject_code",col_Names) | grepl("activity_code",col_Names) | grepl("mean",col_Names) | grepl("mean",col_Names) )
 
 Mean_StanDevn <- merged_all[,logical_Mean_StdDevn==TRUE]
 
-#Naming------------------
+#Replacing the values of activity_code(1-6) & subject_code)(1-30) to the corresponding characters 
 
 Mean_StanDevn$activity_code <- factor(Mean_StanDevn$activity_code,levels=activity_labels[,1],labels=activity_labels[,2])
 Mean_StanDevn$subject_code<- as.factor(Mean_StanDevn$subject_code)
 
-#labeling-----------
+#For a specific activity of a particular subject, mean has been taken for all the values corresponding to that 
+
 melted_mean <- melt(Mean_StanDevn,id=c("subject_code","activity_code"))
 melted_mean <- dcast(melted_mean, subject_code + activity_code ~ variable,mean)
 
-#Final Tidy dataset
+#Final Tidy dataset exported as "Tidy_mean.txt"
 
 write.table(melted_mean, file = "Tidy_mean.txt",row.names = FALSE, quote = FALSE)
 
